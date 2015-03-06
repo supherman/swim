a897ee23f444cb448bbd6fbfab0b1c6655386131
Herman Moreno <herman.moreno@crowdint.com>
2015-03-06 14:22:16 -0600

    Make cli simpler

--- swim.go
+++ swim.go
@@ -6,8 +6,8 @@ import (
 	"github.com/supherman/swim/server"
 )
 
-var port = flag.String("port", "0", "3000")
-var peer = flag.String("peer", "", "127.0.0.1:4000")
+var port = flag.String("port", "", "3000")
+var peer = flag.String("peer", "", "4000")
 
 func main() {
 	flag.Parse()
